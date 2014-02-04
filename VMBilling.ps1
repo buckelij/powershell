5a40ae357d38a669f6fb56548d502ffa595e4be0
Elijah Buck <elijah.buck@gmail.com>
2014-02-04 09:54:44 -0500

    add VMBilling script

---
+++ VMBilling.ps1
@@ -0,0 +1,21 @@
+echo "VMBilling.ps1 : Generates a list of VMs sorted by the Folder they reside in
+      and saves the output as a CSV to your Desktop. Requires the VMWare PowerCLI Snapin
+      
+      You may be prompted for credentials to access VCenter."
+
+# Elijah Buck  - 4Feb2014 - Initial revision
+	  
+Add-PSSnapin VMware.VimAutomation.Core
+
+if ( (Get-PowerCLIConfiguration -Scope User).InvalidCertificateAction -eq $null ) {
+  Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Scope User
+}
+
+Connect-VIServer -Server vcenter.thejockeyclub.net
+$exportPath = "$home\Desktop\VMBilling-$((get-date -Format s) -replace ':',".").csv"
+$vms = get-vm
+$vmexport = $vms | select Name,Folder,@{ name="DataCenter"; expression = {Get-DataCenter -VM $_}},`
+                          MemoryGB,NumCPU,`
+                          @{name="diskGB"; expression = {[math]::floor($_.ProvisionedSpaceGB)}}`
+                          ,PowerState
+$vmexport | Sort Folder | Export-CSV -Path $exportPath -Encoding ASCII
